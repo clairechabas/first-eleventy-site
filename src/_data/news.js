eb32eda20a8fbd8dd855d02198d3d598f37c962d
Chabou <claire.chabas@gmail.com>
2020-07-22 15:06:47 +0200

    Adding localized news pages for each available country, default page and 404 page

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,13 +1,25 @@
 const axios = require('axios');
+const countries = require('./countries.json');
 require('dotenv').config();
 
-module.exports = async function () {
+async function getNewsFrom(country) {
   try {
     const res = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=fr&apiKey=${process.env.NEWS_API}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API}&pageSize=5`
     );
-    return res.data;
+    return {
+      country: country,
+      articles: res.data.articles,
+    };
   } catch (err) {
     console.log(err);
   }
+}
+
+module.exports = async function () {
+  var newsPromises = countries.map(getNewsFrom);
+  return Promise.all(newsPromises).then((news) => {
+    console.log('news :', news);
+    return [].concat.apply([], news);
+  });
 };
